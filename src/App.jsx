79e4fe1c16b43a68445b6cea798292e666882e53
sabbir00921 <sabbir00921@gmail.com>
2025-04-29 05:35:54 +0600

    added base

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import SignIn from './pages/signIn/Index'
 import Signup from './pages/signup/index';
-import RootLayout from './components/rootLayout/RootLayout';
+
 import Home from './pages/Home/Index';
 import Message from './pages/Message/index';
 import Notification from './pages/Notification/index';
 import Error from './pages/Error/Error';
 import { BrowserRouter, Routes, Route } from "react-router";
+import RootLayout from './components/RootLayout/RootLayout';
 
 const App = () => {
   return (
